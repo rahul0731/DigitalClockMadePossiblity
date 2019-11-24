5666c5bc66db3637cf9ca7d1d1c4ea37fc280bd8
rahul0731 <38300938+rahul0731@users.noreply.github.com>
2019-11-24 13:06:57 +0530

    Add files via upload

---
+++ DigitalClock.java
@@ -0,0 +1,64 @@
+package Important;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DigitalClock {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 3;
+		int c = 3;
+		int d = 2;
+		int total_valid_Time = digitalClock(a, b, c, d);
+		System.out.println(set);
+		System.out.println(total_valid_Time);
+		
+	}  
+
+	static int digitalClock(int a, int b, int c, int d) {
+		int arr[] = new int[4];
+		arr[0] = a;
+		arr[1] = b;
+		arr[2] = c;
+		arr[3] = d;
+		permute(arr, 0);
+		return set.size();
+	}
+
+	static ArrayList<String> al = new ArrayList();
+	static Set<String> set = new HashSet<String>();;
+
+	static void permute(int[] a, int k) {
+		if (k == a.length) {
+			for (int i = 0; i < a.length; i++) {
+				String str1 = a[0] + "" + a[1];
+				String str2 = a[2] + "" + a[3];
+				int hh = Integer.parseInt(str1);
+				int mm = Integer.parseInt(str2);
+				if (hh <= 23 && mm <= 59) {
+					String s = hh + ":" + mm;
+					al.add(s);
+				}
+
+			}
+		} else {
+			for (int i = k; i < a.length; i++) {
+				int temp = a[k];
+				a[k] = a[i];
+				a[i] = temp;
+
+				permute(a, k + 1);
+
+				temp = a[k];
+				a[k] = a[i];
+				a[i] = temp;
+			}
+		}
+		set.addAll(al);
+
+	}
+
+}
